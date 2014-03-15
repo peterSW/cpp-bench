a5b2d59a25f34261cd321db7d2e6cb052ec9d314
Peter Stroia-Williams <peterDwilliams@gmail.com>
2014-03-15 17:08:06 +0000

    Adds time observation collection.

--- tests/unit/TestMockTimedFunctors.cc
+++ tests/unit/TestMockTimedFunctors.cc
@@ -1,5 +1,6 @@
 #include "MockTimer.h"
 #include "MockTimedFunctors.h"
+#include <vector>
 #include <gmock/gmock.h>
 
 using namespace testing;
@@ -38,4 +39,67 @@ TEST_F(MockTimedFunctors, CanSimulateTimingOfSqrFunc)
     mockTimer.start();
     sqr_mtf(problemSize);
     EXPECT_THAT(mockTimer.elapsedTime(), Eq(problemSize*problemSize));
+}
+
+
+struct Observation
+{
+    size_t problemSize;
+    double time;
+};
+typedef std::vector<Observation> Observations;
+
+class ObservationsCollector
+{
+public:
+    ObservationsCollector(Timer &timer)
+    :
+        m_timer(timer)
+    {
+    }
+    void setProbSizes(const std::vector<size_t> &sizes)
+    {
+        m_probSizes = sizes;
+    }
+
+    template <typename Functor>
+    Observations observationsOf(Functor &func)
+    {
+        Observations obs(m_probSizes.size());
+        for(size_t i(0); i < m_probSizes.size(); ++i)
+        {
+            obs[i] = makeObservation(func, m_probSizes[i]);
+        }
+        return obs;
+    }
+
+    template <typename Functor>
+    Observation makeObservation(Functor &func, size_t probSize)
+    {
+        Observation obs;
+        m_timer.start();
+        func(probSize);
+        obs.time = m_timer.elapsedTime();
+
+        obs.problemSize = probSize;
+        return obs;
+    }
+private:
+    Timer &m_timer;
+    std::vector<size_t> m_probSizes;
+};
+
+TEST_F(MockTimedFunctors, canCollect)
+{
+    ObservationsCollector obsCollector(mockTimer);
+    std::vector<size_t> probSizes;
+    probSizes.push_back(1);
+    probSizes.push_back(5);
+    obsCollector.setProbSizes(probSizes);
+    const Observations obs(obsCollector.observationsOf(lin_mtf));
+
+    for(size_t i(0); i < obs.size(); ++i)
+    {
+        EXPECT_THAT(obs[i].problemSize, Eq(obs[i].time));
+    }
 }
